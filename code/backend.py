318a9f8f4243f5356195aeeb10b9f05ea2946f21
Maitreya152 <maitreya.chitale@research.iiit.ac.in>
2024-02-17 02:32:44 +0530

    Enter Sample added

--- code/backend.py
+++ code/backend.py
@@ -2,6 +2,10 @@ import pymongo
 from essentials import clear_screen
 from essentials import TextColors
 
+class Valid_Inputs:
+
+    sampleTypes = ['blood', 'urine', 'saliva']
+
 class Checker:
 
     @staticmethod
@@ -38,13 +42,14 @@ class Checker:
             signup_method()
 
     @staticmethod
-    def check_password_login(username,password,users_collection,login_method):
-        user = users_collection.find_one({'_id': username,'password':password})
+    def check_password_login(username,password, users_collection, login_method):
+
+        user = users_collection.find_one({'_id': username, 'password':password})
         if user:
             clear_screen()
-            return
+            return user
         else:
-            print(f"{TextColors.RED}Incorrect password. Press Enter to continue{TextColors.END}")
+            print(f"{TextColors.RED}Incorrect password and username pair. Press Enter to continue.{TextColors.END}")
             inp = input()
             clear_screen()
             login_method()
@@ -54,12 +59,34 @@ class Checker:
 
         user = users_collection.find_one({'_id': username})
         if user:
-            return
+            return username
         else:
             clear_screen()
-            print(f"{TextColors.RED}Username doesn't exist. Please signup.  Press Enter to continue.{TextColors.END}")
+            print(f"{TextColors.RED}Username doesn't exist. Please signup. Press Enter to continue.{TextColors.END}")
             inp = input()
             signup_method()
+
+    @staticmethod
+    def check_s_id(s_id, samples_collection, enter_sample_method, user_name):
+
+        sample = samples_collection.find_one({'_id': s_id})
+        if sample:
+            print(f"{TextColors.RED}Sample already exists, invalid sample ID. Press enter to re-enter sample ID{TextColors.END}")
+            inp = input()
+            enter_sample_method(user_name)
+        else:
+            return
+        
+    @staticmethod
+    def check_s_type(s_type, enter_sample_method, user_name):
+
+        sample_types = Valid_Inputs.sampleTypes
+        if s_type in sample_types:
+            return
+        else:
+            print(f"{TextColors.RED}Invalid sample type. Press enter to re-enter sample ID{TextColors.END}")
+            inp = input()
+            enter_sample_method(user_name)
 
 class DataBase:
 
@@ -72,6 +99,7 @@ class DataBase:
     def signup(self):
         clear_screen()
         u_name = input(f"{TextColors.BLUE}Enter Username: {TextColors.END}")
+        u_name = u_name.lower()
         Checker.check_uname_signup(u_name, self.users_collection, self.signup)
 
         password = input(f"{TextColors.BLUE}Enter Password: {TextColors.END}")
@@ -101,13 +129,35 @@ class DataBase:
         Checker.check_uname_login(u_name, self.users_collection, self.signup)
 
         password = input(f"{TextColors.BLUE}Enter Password: {TextColors.END}")
-        Checker.check_password_login(u_name,password, self.users_collection, self.login)
+        user = Checker.check_password_login(u_name, password, self.users_collection, self.login)
+        
+        return user
+    
+    def enter_sample(self, user_name):
+
         clear_screen()
-        print(f"{TextColors.GREEN}User logged in successfully.{TextColors.END}")
+        s_id = input(f"{TextColors.BLUE}Enter Sample_ID: {TextColors.END}")
+        Checker.check_s_id(s_id, self.samples_collection, self.enter_sample, user_name)
+
+        s_type = input(f"{TextColors.BLUE}Enter Sample_Type: {TextColors.END}")
+        s_type = s_type.lower()
+        Checker.check_s_type(s_type, self.enter_sample, user_name)
+
+        s_data = input(f"{TextColors.BLUE}Enter the sample data: {TextColors.END}")
+
+        sample_data = {
+            '_id': s_id,
+            's_type': s_type,
+            's_data': s_data,
+            'u_name': user_name
+        }
+        self.samples_collection.insert_one(sample_data)
+        clear_screen()
+        print(f"{TextColors.GREEN}Sample added to the database successfully.{TextColors.END}")
         print()
         print(f"{TextColors.YELLOW}Press enter to continue.{TextColors.END}")
         inp = input()
-        main()
+        return
 
     def close(self):
 
@@ -130,10 +180,37 @@ def main():
 
     if choice == '1':
         database.signup()
+
     elif choice == '2':
-        database.login()
+        user = database.login()
+        while True:
+            clear_screen()
+            print(f"{TextColors.GREEN}Welcome {user['_id']}.{TextColors.END}")
+            print()
+            print(f"{TextColors.YELLOW}1. Enter new sample{TextColors.END}")
+            print(f"{TextColors.YELLOW}2. Settings{TextColors.END}")
+            print(f"{TextColors.YELLOW}3. Logout{TextColors.END}")
+            print()
+            choice = input(f"{TextColors.BLUE}Choose an option from above to proceed: {TextColors.END}")
+
+            if choice == '1':
+                database.enter_sample(user)
+                continue
+
+            elif choice == '2':
+                pass
+
+            elif choice == '3':
+                pass
+
+            else:
+                print(f"{TextColors.RED}Invalid choice, press enter to return to landing page.{TextColors.END}")
+                inp = input()
+                continue
+
     elif choice == '3': 
         pass
+
     else:
         clear_screen()
         print(f"{TextColors.RED}Invalid Choice. Please enter a valid option.{TextColors.END}")
