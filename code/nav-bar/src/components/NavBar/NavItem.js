01a8fd6ee768e9879b25f9cc0d91ce1c0d4fc16e
Maitreya152 <maitreya.chitale@research.iiit.ac.in>
2024-03-19 13:52:24 +0530

    UI Tweaks

--- code/nav-bar/src/components/NavBar/NavItem.js
+++ code/nav-bar/src/components/NavBar/NavItem.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { Link,useNavigate } from "react-router-dom";
 
 function NavItem({name}) {
-    return <div style={{color:"#09db33", background : "#0a1a04",paddingLeft: "15px", paddingRight: "15px", height :"40px",
+    return <div style={{color:"white", background : "#0a1a04",paddingLeft: "15px", paddingRight: "15px", height :"40px",
      justifyContent:"center", textAlign:"center", verticalAlign:"middle", lineHeight:"40px",
      fontFamily:"consolas", fontSize:"20px",
      clipPath: "polygon( 15px 0, calc(100% - 15px) 0, 100% 50%, calc(100% - 15px) 100%, 15px 100%, 0% 50%)"}}>
