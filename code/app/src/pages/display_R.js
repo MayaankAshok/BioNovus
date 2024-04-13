91462c345a049c964cca1c0e4382fbd096a06dcd
Maitreya152 <maitreya.chitale@research.iiit.ac.in>
2024-04-13 23:53:47 +0530

    start

--- code/app/src/pages/display_R.js
+++ code/app/src/pages/display_R.js
@@ -1,47 +1,61 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import './button.css';
-import { Nav } from '../components/NavBar/NavElements';
-import NavItem from '../components/NavBar/NavItem';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { PDFDownloadLink, Document, Page, Text, StyleSheet } from "@react-pdf/renderer";
+import "./button.css";
+import { Nav } from "../components/NavBar/NavElements";
+import NavItem from "../components/NavBar/NavItem";
 
 function Display_R() {
-  // let items = ["id_1", "id_2", "id_3", "id_4"]; //this is where the items of anything to be displayed will go
   const [items, setResults] = useState([]);
 
   const getResults = async () => {
     try {
-      const response = await axios.get('http://localhost:5000/display_R');
-      const sortedResults = response.data.sort((a, b) => {
-        return a.id.localeCompare(b.id);
-      });
-      console.log(sortedResults);
+      const response = await axios.get("http://localhost:5000/display_R");
+      const sortedResults = response.data.sort((a, b) => a.id.localeCompare(b.id));
       setResults(sortedResults);
     } catch (error) {
-      console.error('Error fetching results:', error);
+      console.error("Error fetching results:", error);
     }
   };
 
   useEffect(() => {
-    getResults(); // Trigger the GET request when the component mounts
+    getResults();
   }, []);
 
+  const MyDocument = ({ items }) => (
+    <Document>
+      <Page style={styles.page}>
+        {items.map((item) => (
+          <Text key={item.id}>
+            {item.id}: {item.type}, {item.intensity}
+          </Text>
+        ))}
+      </Page>
+    </Document>
+  );
+
+  const styles = StyleSheet.create({
+    page: {
+      flexDirection: "column",
+      backgroundColor: "#ffffff",
+      padding: 10,
+    },
+  });
+
   return (
-    <div
-      style={{
-        height: '80vh',
-      }}
-    >
+    <div style={{ height: "80vh" }}>
       <h1>All Results</h1>
       {items.map((item) => (
-        <li
-          className="list"
-          key={item.id}
-          style={{
-            fontSize: '18px',
-          }}
-        >
-          {item.id}: {item.type}
-          <button className="button1"><NavItem name="Print"></NavItem></button>
+        <li className="list" key={item.id} style={{ fontSize: "18px" }}>
+          {item.id}: {item.type}, {item.intensity}
+          <button className="button1">
+            <PDFDownloadLink
+              document={<MyDocument items={items} />}
+              fileName="results.pdf"
+            >
+              {({ loading }) => (loading ? "Loading document..." : "Print")}
+            </PDFDownloadLink>
+          </button>
         </li>
       ))}
     </div>
